3e295b6eaafabaaf94ecc0cfab2f4a0b08945ad9
Dzmitry Mikhayevich <dima.mikhayevich@mail.ru>
2023-02-07 21:28:17 +0300

    feat: 2/4 done 08-objects

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -23,7 +23,13 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    throw new Error('Not implemented');
+    const rect = new Object()
+    rect.height=height;
+    rect.width=width;
+    rect.getArea = function () {
+        return height*width
+    }
+    return rect
 }
 
 
@@ -38,7 +44,7 @@ function Rectangle(width, height) {
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
 function getJSON(obj) {
-    throw new Error('Not implemented');
+    return JSON.stringify(obj)
 }
 
 
@@ -54,7 +60,10 @@ function getJSON(obj) {
  *
  */
 function fromJSON(proto, json) {
-    throw new Error('Not implemented');
+    const obj = JSON.parse(json);
+    const values = Object.values(obj);
+
+    return new proto.constructor(...values);
 }
 
 
